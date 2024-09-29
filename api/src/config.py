09c8c01e3e7c28ec6bef70c3e939f893fef4e4c5
Jo Kroese <jo@jokroese.com>
2024-09-30 00:21:09 +0200

    use default model if none specified
    this is useful when running the api manually

--- api/src/config.py
+++ api/src/config.py
@@ -1,10 +1,12 @@
 import os
+import warnings
 
 
 def get_model_name():
     model_name = os.getenv("OLLAMA_MODEL")
     if not model_name:
-        raise EnvironmentError(
-            "The OLLAMA_MODEL environment variable is not defined. Please set it to the desired model."
+        warnings.warn(
+            "The OLLAMA_MODEL environment variable is not defined. Using the default model."
         )
+        model_name = "qwen2.5:0.5b"
     return model_name
